a48d8a311e9957c92ae88ea27ec67304cac85d9d
Hey_Tack <jsisk415@gmail.com>
2024-02-22 21:13:45 -0600

    Tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
-  };
-  
-  export default nextConfig;
+    output: 'export'
+  }
+   
+  module.exports = nextConfig
